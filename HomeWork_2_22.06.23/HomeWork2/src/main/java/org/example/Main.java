560ff37a8b5b9db6e383c2639f14ac892dae071f
BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>
2023-06-29 13:19:42 +0200

    Add files via upload
    HomeWork from Basic Java #2 (22.06.2023)

---
+++ HomeWork_2_22.06.23/HomeWork2/src/main/java/org/example/Main.java
@@ -0,0 +1,44 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("hello");
+        char Char = 'G';
+        int myInt = 89;
+        byte myByte = 4;
+        short myShort = 56;
+        float myFloat = 4.7333436F;
+        double myDouble = 4.355453532;
+        long myLong = 12121;
+
+        System.out.println("Character = " + Char);
+        System.out.println("Integer = " + myInt);
+        System.out.println("Byte =" + myByte);
+        System.out.println("Short = " + myShort);
+        System.out.println("Float = " + myFloat);
+        System.out.println("Double= " +myDouble);
+        System.out.println("Long = " + myLong);
+
+        Character myCharacter = 'G';
+        Integer myInteger = 89;
+        Byte myByte1 = 4;
+        Short myShort2 = 56;
+        Float myFloat1 = 4.7333436F;
+        Double myDouble1 = 4.355453532;
+        Long myLong1 = 12121L;
+
+
+        System.out.println( Char);
+        System.out.println( myInt);
+
+        System.out.println(myByte);
+        System.out.println( myShort);
+        System.out.println(myFloat);
+        System.out.println(myDouble);
+        System.out.println(myLong);
+
+        System.out.println(myFloat);
+        System.out.println(myDouble);
+    }
+}
