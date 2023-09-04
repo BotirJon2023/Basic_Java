b7213cebd18377f751cfcf81dfdf1de38f46cd95
BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>
2023-09-04 18:40:52 +0200

    Add files via upload
    Learning Java programming language

---
+++ Lesson_2/Project_2.java
@@ -0,0 +1,63 @@
+package lesson;
+
+class Example {
+public static void main(String[] args) {
+    String firstString = "Это ферари красного цвета";
+    String firstString = "Это ферари синего цвета";
+    String secondString = "Это ферари черного цвета";
+    String secondtring = "Это ферари желтого цвета";
+    String secondtring = "Это ферари оранжевого цвета";
+
+    String thirdString = new String ("Это ферари зеленого цвета");
+
+    System.out.println(firstString);
+
+
+    int
+
+
+new String(original "asdas")
+
+}
+}
+
+class Vasya {
+    public static void main(String[] args) {
+        int thisIsThePacage = 1;
+
+    }
+}
+
+class Data {
+    public int count = 0;
+
+    public static void main(String[] args) {
+        Data data = new Data();
+        System.out.println()
+
+    }
+
+    public void examples() {
+        String myName = "Ivan";
+    }
+}
+
+
+
+
+
+
+
+package scanner;
+
+import java.unit.Scanner;
+
+public class Main {
+
+    public static void Main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("This is from Keyboard" + scanner.nextLine());
+    }
+
+}
