9e79afef8a95b419458b46c75264deb652a16e4f
BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>
2023-10-30 08:32:57 +0100

    Add files via upload
    Learning Java Programming Language

---
+++ Lesson_a13.29.09.23/LessonA13/src/main/java/org/example/CompareByAgeDecreasing.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.Comparator;
+
+public class CompareByAgeDecreasing implements Comparator<Monkey> {
+    @Override
+    public int compare(Monkey o1, Monkey o2) {
+        return o2.getAge() - o1.getAge();
+    }
+}
