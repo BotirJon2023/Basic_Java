bcec623bd73f5130eea60789e76270ae4b5d06eb
BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>
2023-11-07 08:57:01 +0100

    Add files via upload
    Learning Java Programming Language...

---
+++ Lesson_a18.11.10.23/LessonA18/src/main/java/org/example/LessonA18.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class LessonA18 {
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
---
+++ Lesson_a18.11.10.23/LessonB18/src/main/java/org/example/JacksonProtobufMain.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.protobuf.ProtobufMapper;
+import com.fasterxml.jackson.dataformat.protobuf.schema.ProtobufSchema;
+
+public class JacksonProtobufMain {
+    public static void main(String[] args) throws Throwable {
+        ProtobufMapper om = new ProtobufMapper();
+        ProtobufSchema schema = om.generateSchemaFor(Person.class);
+
+        Person person = new Person("Вася", "Пупкин",
+                new Address(7, "Н", "Бассейная"));
+        byte[] bytes = om.writer(schema).writeValueAsBytes(person);
+
+        Person read = om.readerFor(Person.class)
+                .with(schema)
+                .readValue(bytes);
+        System.out.printf("Read person: %s\n", read);
+    }
+
+    private static class ProtobufMapper {
+    }
+}
