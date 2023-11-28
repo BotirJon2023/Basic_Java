3f488a52487c3dc7fb7c8ab766aab02862765408
BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>
2023-11-28 13:24:57 +0100

    Add files via upload
    Learning Java Programming Language...

---
+++ Lesson_12_28.11.23/Lesson12/src/main/java/org/example/Main.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import java.util.Collections;
+import java.util.ArrayList;
+
+class Main {
+    public static void main(String[] args) {
+        // Creating an ArrayList
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(9);
+        numbers.add(7);
+        numbers.add(8);
+        numbers.add(6);
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(10);
+        numbers.add(3);
+        System.out.println("ArrayList1: " + numbers);
+
+        // Using reverse()
+        Collections.reverse(numbers);
+        System.out.println("Reversed ArrayList1: " + numbers);
+
+        // Using swap()
+        Collections.swap(numbers, 0, 1);
+        System.out.println("ArrayList1 using swap(): " + numbers);
+
+        ArrayList<Integer> newNumbers = new ArrayList<>();
+
+        // Using addAll
+        newNumbers.addAll(numbers);
+        System.out.println("ArrayList2 using addAll(): " + newNumbers);
+
+        // Using fill()
+        Collections.fill(numbers, 0);
+        System.out.println("ArrayList1 using fill(): " + numbers);
+
+        // Using copy()
+        Collections.copy(newNumbers, numbers);
+        System.out.println("ArrayList2 using copy(): " + newNumbers);
+    }
+}
