748897259d24fdaea63219d627ba10f73753fe1b
BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>
2023-11-30 09:22:25 +0100

    Add files via upload
    Learning Java Programming Language

---
+++ Lesson_a30.30.10.23/LessonA30/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayService service = new ArrayService();
+
+        int[] array = service.getArrayBySize(7);
+
+        System.out.println(Arrays.toString(array));
+    }
+}
