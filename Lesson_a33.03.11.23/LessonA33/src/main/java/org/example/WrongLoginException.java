b75ea919aea605f8678e2130fa040f5f3bd2cc05
BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>
2023-12-05 09:00:53 +0100

    Add files via upload
    Learning Java Programming Language.

---
+++ Lesson_a33.03.11.23/LessonA33/src/main/java/org/example/WrongLoginException.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class WrongLoginException extends Exception {
+
+
+    public WrongLoginException() {
+    }
+
+    public WrongLoginException(String message) {
+        super(message);
+    }
+
+}
---
+++ Lesson_a33.03.11.23/LessonA33/src/test/java/org/example/LoginAndPasswordCheckerTest.java
@@ -0,0 +1,31 @@
+package org.example;
+
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class LoginAndPasswordCheckerTest {
+
+    private static LoginAndPasswordChecker checker;
+
+    @BeforeAll
+    public static void init(){
+        checker = new LoginAndPasswordChecker();
+    }
+
+    @Test
+    public void checkLogin(){
+
+    }
+
+    @Test
+    public void checkPassword(){
+
+    }
+
+    @Test
+    public void checkConfirmPassword(){
+
+    }
+
+}
