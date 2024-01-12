1dbde462f690d0b43db0b4a9c59e93a7433ad251
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-01-12 09:46:30 +0100

    Add files via upload
    Learning Java Programming Language.

---
+++ Lesson_a43.20.11.23/LessonA43/src/main/java/org/example/MT3.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class MT3 {
+    public static void main(String[] args) {
+        new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                System.out.println("TH1: " + i + " ***");
+            }
+        }).start();
+    }
+}
