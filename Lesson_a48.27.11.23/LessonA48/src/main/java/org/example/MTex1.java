fb4e7cf689813a6ee117963b90949b246817489b
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-01-19 09:32:12 +0100

    Add files via upload
    Learning Java Programming Language.

---
+++ Lesson_a48.27.11.23/LessonA48/src/main/java/org/example/MTex1.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class MTex1 {
+    public static void main(String[] args) {
+        /**
+         * wait() - поток ждут до тех пор пока не будет
+         * вызван notify() or notifyAll()
+         *
+         * notify() - будит пред wait()
+         * notifyAll() - будит все
+         */
+
+
+    }
+}
