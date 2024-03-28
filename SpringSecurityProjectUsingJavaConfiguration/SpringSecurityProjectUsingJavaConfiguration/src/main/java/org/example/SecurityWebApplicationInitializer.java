fbd51c30005c9e8bd6d27e74d1364e6abaa225fa
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-03-28 10:18:41 +0100

    Add files via upload
    Learning Java Programming Language.

---
+++ SpringSecurityProjectUsingJavaConfiguration/SpringSecurityProjectUsingJavaConfiguration/src/main/java/org/example/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package org.example;
+
+import org.springframework.security.web.context.*;
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+    }
---
+++ SpringSecurityProjectUsingJavaConfiguration/SpringSecurityProjectUsingJavaConfiguration/src/main/java/org/example/WebSecurityConfigurerAdapter.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class WebSecurityConfigurerAdapter {
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+    }
+
+}
