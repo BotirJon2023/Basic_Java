a6450ce8544c13a957ca72ccdab7487dbbae8a0b
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-06-21 05:58:12 +0200

    Add files via upload
    Space manipulation in string by Java code example

---
+++ ReplaceTheSpacesOfAStringWithASpecificCharacter/ReplaceTheSpacesOfAStringWithASpecificCharacter/src/main/java/org/example/ReplaceTheSpacesOfAStringWithASpecificCharacter.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class ReplaceTheSpacesOfAStringWithASpecificCharacter {
+
+    public static void main(String[] args) {
+
+        String string = "Once in a blue moon";
+
+        char ch = '-';
+
+
+        string = string.replace(' ', ch);
+
+        System.out.println("String after replacing spaces with given character: ");
+
+        System.out.println(string);
+    }
+}
