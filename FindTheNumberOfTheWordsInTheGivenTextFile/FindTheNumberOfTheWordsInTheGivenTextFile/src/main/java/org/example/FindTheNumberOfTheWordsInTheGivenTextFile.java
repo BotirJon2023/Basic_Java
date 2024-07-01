4a5a0bed85c47e3f40d30fd6fe485d1ca76676eb
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-07-01 02:13:30 +0200

    Add files via upload
    Number of words in string with Java code example

---
+++ FindTheNumberOfTheWordsInTheGivenTextFile/FindTheNumberOfTheWordsInTheGivenTextFile/src/main/java/org/example/FindTheNumberOfTheWordsInTheGivenTextFile.java
@@ -0,0 +1,33 @@
+package org.example;
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FindTheNumberOfTheWordsInTheGivenTextFile extends IOException {
+
+    public static void main(String[] args) throws Exception {
+
+        String line;
+
+        int count = 0;
+
+
+        FileReader file = new FileReader("data.txt ");
+
+        BufferedReader br = new BufferedReader(file);
+
+
+        while ((line = br.readLine()) != null) {
+
+            String words[] = line.split("");
+
+            count = count + words.length;
+        }
+
+        System.out.println("Number of words present in given file: " + count);
+
+        br.close();
+    }
+}
