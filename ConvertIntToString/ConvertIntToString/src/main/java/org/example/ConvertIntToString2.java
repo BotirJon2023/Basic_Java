aae97c7f959815f77801d93234b706ad3e7fbe01
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-07-13 05:43:40 +0200

    Add files via upload
    Converting integer to string with Java code examples

---
+++ ConvertIntToString/ConvertIntToString/src/main/java/org/example/ConvertIntToString2.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class ConvertIntToString2 {
+
+    public static void main(String args[]) {
+
+        int i = 200;
+
+        String s = String.format("%d", i);
+
+        System.out.println(s);
+    }
+}
