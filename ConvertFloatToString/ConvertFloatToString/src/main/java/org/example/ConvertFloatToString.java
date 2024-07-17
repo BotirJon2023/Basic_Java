8662fec2896c629f9b800d49df200fd25db85b95
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-07-17 11:10:49 +0200

    Add files via upload
    Converting float to string with Java code examples

---
+++ ConvertFloatToString/ConvertFloatToString/src/main/java/org/example/ConvertFloatToString.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class ConvertFloatToString {
+
+    public static void main(String args[]) {
+
+        float f = 12.3F;
+
+        String s = String.valueOf(f);
+
+        System.out.println(s);
+    }
+}
---
+++ ConvertFloatToString/ConvertFloatToString/src/main/java/org/example/ConvertFloatToString2.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class ConvertFloatToString2 {
+
+    public static void main(String args[]) {
+
+        float f = 89.7F;
+
+        String s = Float.toString(f);
+
+        System.out.println(s);
+    }
+}
