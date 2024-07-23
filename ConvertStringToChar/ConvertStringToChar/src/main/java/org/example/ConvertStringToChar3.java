1ab68f648fe252482fbdef2ab13c9b632ac97ee8
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-07-23 12:33:59 +0200

    Add files via upload
    Converting string to char with Java code example

---
+++ ConvertStringToChar/ConvertStringToChar/src/main/java/org/example/ConvertStringToChar3.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class ConvertStringToChar3 {
+
+    public static void main(String args[]) {
+
+        String s1 = "hello";
+
+        char[] ch = s1.toCharArray();
+
+        for (int i = 0; i < ch.length; i++) {
+
+            System.out.println("char at " + i + " index is: " + ch[i]);
+        }
+    }
+}
