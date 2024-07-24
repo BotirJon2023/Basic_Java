5108e24ff1be1d8f0d6792cad00a108c47af5599
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-07-24 10:43:26 +0200

    Add files via upload
    Converting char to string with Java code example

---
+++ ConvertCharToString/ConvertCharToString/src/main/java/org/example/ConvertCharToString.java
@@ -0,0 +1,14 @@
+package org.example;
+
+
+public class ConvertCharToString {
+
+    public static void main(String args[]) {
+
+        char c = 'S';
+
+        String s = String.valueOf(c);
+
+        System.out.println("String is: " + s);
+    }
+}
---
+++ ConvertCharToString/ConvertCharToString/src/main/java/org/example/ConvertCharToString2.java
@@ -0,0 +1,14 @@
+package org.example;
+
+
+public class ConvertCharToString2 {
+
+    public static void main(String args[]) {
+
+        char c = 'M';
+
+        String s = Character.toString(c);
+
+        System.out.println("String is: " + s);
+    }
+}
