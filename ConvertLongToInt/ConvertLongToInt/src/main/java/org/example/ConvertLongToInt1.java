173b74e67871c0f9d7aa6c9875291a144cced936
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-07-29 23:39:20 +0200

    Add files via upload
    Converting long to integer with Java code example

---
+++ ConvertLongToInt/ConvertLongToInt/src/main/java/org/example/ConvertLongToInt1.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class ConvertLongToInt1 {
+
+    public static void main(String args[]) {
+
+        long l = 500;
+
+        int i = (int) l;
+
+        System.out.println(i);
+    }
+}
---
+++ ConvertLongToInt/ConvertLongToInt/src/main/java/org/example/ConvertLongToInt2.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class ConvertLongToInt2 {
+
+    public static void main(String args[]) {
+
+        Long l = new Long(10);
+
+        int i = l.intValue();
+
+        System.out.println(i);
+    }
+}
