16640b377bea9bfc92bed363c9fbff750dbc878d
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-08-01 20:42:20 +0200

    Add files via upload
    Converting char to integer with Java code experience

---
+++ ConvertCharToInt/ConvertCharToInt/src/main/java/org/example/ConvertCharToInt2.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class ConvertCharToInt2 {
+
+    public static void main(String args[]) {
+
+        char c = '1';
+
+        int a = Character.getNumericValue(c);
+
+        System.out.println(a);
+    }
+}
---
+++ ConvertCharToInt/ConvertCharToInt/src/main/java/org/example/ConvertCharToInt3.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class ConvertCharToInt3 {
+
+    public static void main(String args[]) {
+
+        char c = '1';
+
+        int a = Integer.parseInt(String.valueOf(c));
+
+        System.out.println(a);
+    }
+}
