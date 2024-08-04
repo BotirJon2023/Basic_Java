cfbe6d77c48c7d045a12fec43f0f36e65f1543c2
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-08-04 10:07:13 +0200

    Add files via upload
    Convert boolean to string with Java code example

---
+++ ConvertBooleanToString/ConvertBooleanToString/src/main/java/org/example/ConvertBooleanToString2.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class ConvertBooleanToString2 {
+
+    public static void main(String args[]) {
+
+        boolean b1 = true;
+
+        boolean b2 = false;
+
+        String s1 = Boolean.toString(b1);
+
+        String s2 = Boolean.toString(b2);
+
+        System.out.println(s1);
+
+        System.out.println(s2);
+    }
+}
