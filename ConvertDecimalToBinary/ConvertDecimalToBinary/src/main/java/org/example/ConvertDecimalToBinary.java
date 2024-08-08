77bc110e721027f97cb7957b0d99c7999a8fc984
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-08-08 20:29:53 +0200

    Add files via upload
    Convert decimal to binary with Java code example

---
+++ ConvertDecimalToBinary/ConvertDecimalToBinary/src/main/java/org/example/ConvertDecimalToBinary.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class ConvertDecimalToBinary {
+
+    public static void main(String args[]) {
+
+        System.out.println(Integer.toBinaryString(10));
+
+        System.out.println(Integer.toBinaryString(21));
+
+        System.out.println(Integer.toBinaryString(31));
+    }
+}
