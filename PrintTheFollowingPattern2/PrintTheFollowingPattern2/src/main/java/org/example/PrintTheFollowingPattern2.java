f18d0ff12896530b0f4787be3725ae2ec8349865
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-08-15 09:31:37 +0200

    Add files via upload
    Printing pattern with Java code example

---
+++ PrintTheFollowingPattern2/PrintTheFollowingPattern2/src/main/java/org/example/PrintTheFollowingPattern2.java
@@ -0,0 +1,47 @@
+package org.example;
+
+public class PrintTheFollowingPattern2 {
+
+    public static void main(String[] args) {
+
+        int lines = 4;
+
+        int i, j;
+
+        for (i = 1; i <= lines; i++) {
+
+            for (j = 1; j <= lines; j++) {
+
+                if (i == j)
+
+                    System.out.print("*");
+
+                else
+
+                    System.out.print("0");
+
+            }
+
+            j--;
+
+            System.out.print("*");
+
+            while (j >= 1) {// this loop is used to print * in a line
+
+                if (i == j)
+
+                    System.out.print("*");
+
+                else
+
+                    System.out.print("0");
+
+                j--;
+
+            }
+
+            System.out.println("");
+
+        }
+    }
+}
