f83adf523926d289352001c0c7510e05b398e8c9
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-09-13 06:51:41 +0200

    Add files via upload
    Calculating area with Java coding example

---
+++ AreaOfCircle/AreaOfCircle/src/main/java/org/example/Area.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Area {
+
+    double area;
+    Area(double r)
+    {
+        area= (22*r*r)/7;
+
+    }
+
+}
--- AreaOfCircle/AreaOfCircle/src/main/java/org/example/AreaOfCircle.java
+++ AreaOfCircle/AreaOfCircle/src/main/java/org/example/AreaOfCircle.java
@@ -6,12 +6,11 @@ public class AreaOfCircle {
 
     public static void main(String args[])
     {
-
         Scanner s= new Scanner(System.in);
-
         System.out.println("Enter the radius:");
-        double r= s.nextDouble();
-        double  area=(22*r*r)/7 ;
-        System.out.println("Area of Circle is: " + area);
+        double rad= s.nextDouble();
+        Area  a=new Area(rad);
+        System.out.println("Area of Circle is: " + a.area);
     }
+
 }
