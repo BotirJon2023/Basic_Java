eb0d5e7955df8d8c40a4d0353d70a9bdb05ba7b2
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-09-18 17:03:09 +0200

    Add files via upload
    Counting salary with Java code example

---
+++ SalaryOfEmployee/SalaryOfEmployee/src/main/java/org/example/SalaryOfEmployee.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class SalaryOfEmployee {
+
+    public static void main(String arg[]) {
+        double grossSalary = 10000;
+
+        double incomeTax = 1000;
+
+        double providentFund = 800;
+
+        double professionalTax = 500;
+
+        double netSalary = grossSalary - (incomeTax + providentFund + professionalTax);
+
+        System.out.println("Net Salary is=" + netSalary);
+    }
+}
