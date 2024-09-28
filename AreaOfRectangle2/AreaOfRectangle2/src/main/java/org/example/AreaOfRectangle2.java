2327fdd2fc81bb85d6190ccf57c92acc47626661
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-09-28 21:17:16 +0200

    Add files via upload
    Area of rectangle with Java code example

---
+++ AreaOfRectangle2/AreaOfRectangle2/src/main/java/org/example/AreaOfRectangle2.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class AreaOfRectangle2 {
+
+    public static void main(String args[]) {
+
+        System.out.println("The value of length:");
+
+        double l = 5;
+
+        System.out.println("The value of Breadth:");
+
+        double b = 10;
+
+        double area = l * b;
+
+        System.out.println("Area of Rectangle is: " + area);
+
+    }
+}
