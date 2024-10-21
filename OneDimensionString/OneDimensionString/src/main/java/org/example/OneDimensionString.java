4d1ca6aea08abebf29169f73c6602a86008ce654
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-10-21 22:55:26 +0200

    Add files via upload
    One dimension string with Java code example

---
+++ OneDimensionString/OneDimensionString/src/main/java/org/example/OneDimensionString.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class OneDimensionString {
+
+    public static void main(String[] args) {
+
+
+        String[] str = new String[]{"one", "two", "three", "four"};
+
+        System.out.println("These are elements of one Dimensional array.");
+
+        for (int i = 0; i < str.length; i++) {
+
+            System.err.println(str[i] + "   ");
+
+        }
+    }
+}
