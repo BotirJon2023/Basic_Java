093d20cd76e2500c8a997cf1d124389b6ec6f1c5
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-12-17 21:42:12 +0100

    Add files via upload
    Perform sorting using b tree with Java code example

---
+++ PerformSortingUsingBTree/PerformSortingUsingBTree/src/main/java/org/example/BSTNode.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class BSTNode {
+    public BSTNode left;
+    public BSTNode right;
+
+    public BSTNode(int data) {
+    }
+
+    public int getData() {
+        return 0;
+    }
+
+    public BSTNode getLeft() {
+        return null;
+    }
+
+    public BSTNode getRight() {
+        return null;
+    }
+
+    public void setLeft(BSTNode lt) {
+    }
+
+    public void setRight(BSTNode n) {
+    }
+}
